895325e9ae2631f086ecc48456fcd349dd0a2f9d
Luke Roberts <email@luke-roberts.co.uk>
2016-01-20 17:12:14 -0800

    rm db/seeds

--- script/app.sh
+++ script/app.sh
@@ -3,8 +3,7 @@
 if [ $RACK_ENV = "development" ]
 then
   bundle check || bundle install
+  bundle exec rake db:migrate
 fi
 
-bundle exec rake db:migrate
-bundle exec rake db:seed
 bundle exec rackup -p ${1:-8080}
