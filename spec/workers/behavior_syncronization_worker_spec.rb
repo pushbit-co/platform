c6def51a6baab701d63e7b6fd50cbc20e012a114
Tom Moor <tom.moor@gmail.com>
2016-01-21 13:25:03 -0800

    Added extra fields to config, towards closing #8

--- spec/workers/behavior_syncronization_worker_spec.rb
+++ spec/workers/behavior_syncronization_worker_spec.rb
@@ -18,6 +18,8 @@ describe "perform" do
       expect(Pushbit::Behavior.count).to eql(2)
       expect(Pushbit::Behavior.active.count).to eql(2)
       expect(Pushbit::Behavior.find_by(kind: 'bundler-audit').name).to eql('Bundler Audit')
+      expect(Pushbit::Behavior.find_by(kind: 'bundler-audit').repository_url).to eql('http://example.com')
+      expect(Pushbit::Behavior.find_by(kind: 'bundler-audit').author_name).to eql('Alex')
       expect(Pushbit::Behavior.find_by(kind: 'bundler-update').name).to eql('Bundler Update')
     end
   end
@@ -68,22 +70,6 @@ describe "perform" do
         tone: 'negative',
         active: true
       )
-    end
-
-    it "removes missing behavior" do
-      stub_request(:get, "https://api.github.com/orgs/pushbit-behaviors/repos?per_page=100").
-        to_return(:status => 200, :body => File.read('spec/fixtures/github/webmock/pushbit_behaviors_repos.json'), :headers => {'Content-Type'=>'application/json'})
-
-      stub_request(:get, "https://raw.githubusercontent.com/pushbit-behaviors/bundler-audit/master/config.yml").
-        to_return(:status => 200, :body => File.read('spec/fixtures/github/webmock/bundler_audit_config.yml'), :headers => {})
-
-      stub_request(:get, "https://raw.githubusercontent.com/pushbit-behaviors/bundler-update/master/config.yml").
-        to_return(:status => 200, :body => File.read('spec/fixtures/github/webmock/bundler_update_config.yml'), :headers => {})
-
-      worker.perform
-      expect(Pushbit::Behavior.count).to eql(2)
-      expect(Pushbit::Behavior.active.count).to eql(2)
-      expect(Pushbit::Behavior.find_by(kind: 'filecop')).to eql(nil)
     end
   end
 end
