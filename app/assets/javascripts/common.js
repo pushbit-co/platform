782138dc2ae0b7f50c616d51f7b4673196b58f9b
Tom Moor <tom.moor@gmail.com>
2016-11-06 19:24:04 -0800

    Closing alerts now works More language fixes

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,11 +2,18 @@ import jstz from 'jstz';
 import $ from 'jquery';
 import {addCSRFField} from './libs/forms';
 
-export default {
+const Common = {
   init: () => {
     const timezoneName = jstz.determine().name();
     document.cookie = `timezone=${timezoneName}`;
 
     $(document).on('submit', 'form', ev => addCSRFField(ev));
+    $(document).on('click', '.alert .close', Common.hideBanner);
+  },
+
+  hideBanner: () => {
+    $('.alert').fadeOut();
   }
 };
+
+export default Common;
