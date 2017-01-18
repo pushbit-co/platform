3379633320431f28695e9ebc5989b8cc5a6c6729
Luke Roberts <email@luke-roberts.co.uk>
2017-01-18 20:36:51 +0000

    add indexes

--- db/migrate/20161120232157_simplify_tasks.rb
+++ db/migrate/20161120232157_simplify_tasks.rb
@@ -10,6 +10,6 @@ class SimplifyTasks < ActiveRecord::Migration
       t.timestamps  null: false
     end
 
-    add_index :tasks, :behavior
+    add_index :tasks, :behavior, :repo_id, :trigger_id
   end
 end
